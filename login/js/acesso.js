b49d744a5b68e32e3f882c51a6720ef50f08038a
leoneloliveira <102158956+leoneloliveira@users.noreply.github.com>
2023-03-08 16:48:43 -0300

    Add files via upload

---
+++ login/js/acesso.js
@@ -0,0 +1,10 @@
+function acesso(form) {
+    if (form.nome.value == "admim" && form.senha.value == "123") {
+        location = "logomarca.html"
+    }
+    else {
+        location="acessonegado.html"
+    }
+}
+
+
